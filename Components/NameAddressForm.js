52df86c76c64376a314861558c5c715792017c9b
Wesley L. Handy <wesleylhandy@gmail.com>
2018-06-26 17:47:23 -0400

    adding validation to main form

--- Components/NameAddressForm.js
+++ Components/NameAddressForm.js
@@ -14,6 +14,10 @@ canadianProvinces = [["Alberta", "AB"],["British Columbia", "BC"],["Manitoba", "
 usTerritories = [["American Samoa", "AS"],["Federated States Of Micronesia", "FM"],["Guam", "GU"],["Marshall Islands", "MH"],["Palau", "PW"],["Northern Mariana Islands", "MP"],["Puerto Rico", "PR"],["Virgin Islands", "VI"],["Other", "00"]],
 countries = [["Afghanistan", "AF"],["Aland Islands", ""],["Albania", "AL"],["Algeria", "DZ"],["American Samoa", "AS"],["Andorra", "AD"],["Angola", "AO"],["Anguilla", "AI"],["Antigua and Barbuda", "AG"],["Argentina", "AR"],["Armenia", "AM"],["Aruba", "AW"],["Australia", "AU"],["Austria", "AT"],["Azerbaijan", "AZ"],["Bahamas", "BS"],["Bahrain", "BH"],["Bangladesh", "BD"],["Barbados", "BB"],["Belarus", "BY"],["Belgium", "BE"],["Belize", "BZ"],["Benin", "BJ"],["Bermuda", "BM"],["Bhutan", "BT"],["Bolivia", "BO"],["Bosnia and Herzegovina", "BA"],["Botswana", "BW"],["Brazil", "BR"],["British Virgin Islands", "VG"],["Brunei Darussalam", "BN"],["Bulgaria", "BG"],["Burkina Faso", "BF"],["Burundi", "BI"],["Cambodia", "KH"],["Cameroon", "CM"],["Canada", "CA"],["Cape Verde", "CV"],["Cayman Islands", "KY"],["Central African Republic", "CF"],["Chad", "TD"],["Channel Islands", ""],["Chile", "CL"],["China", "CN"],["Hong Kong Spcl. Admin. Region of China", "CN"],["Macao Spcl. Admin. Region of China", "MO"],["Colombia", "CO"],["Comoros", "KM"],["Congo", "CG"],["Cook Islands", "CK"],["Costa Rica", "CR"],["C�te d\'Ivoire", ""],["Croatia", "HR"],["Cuba", "CU"],["Cyprus", "CY"],["Czech Republic", "CZ"],["Democratic People\'s Rep. of Korea", "KR"],["Democratic Republic of the Congo", "CG"],["Denmark", "DK"],["Djibouti", "DJ"],["Dominica", "DM"],["Dominican Republic", "DO"],["Ecuador", "EC"],["Egypt", "EG"],["El Salvador", "SV"],["Equatorial Guinea", "GQ"],["Eritrea", "ER"],["Estonia", "EE"],["Ethiopia", "ET"],["Faeroe Islands", "FO"],["Falkland Islands (Malvinas)", "FK"],["Fiji", "FJ"],["Finland", "FI"],["France", "FR"],["French Guiana", "GF"],["French Polynesia", "PF"],["Gabon", "GA"],["Gambia", "GM"],["Georgia", "GE"],["Germany", "DE"],["Ghana", "GH"],["Gibraltar", "GI"],["Greece", "GR"],["Greenland", "GL"],["Grenada", "GD"],["Guadeloupe", "GP"],["Guam", "GU"],["Guatemala", "GT"],["Guinea", "GN"],["Guinea-Bissau", "GW"],["Guyana", "GY"],["Haiti", "HT"],["Holy See", ""],["Honduras", "HN"],["Hungary", "HU"],["Iceland", "IS"],["India", "IN"],["Indonesia", "ID"],["Iran (Islamic Republic of)", "IR"],["Iraq", "IQ"],["Ireland", "IE"],["Isle of Man", ""],["Israel", "IL"],["Italy", "IT"],["Jamaica", "JM"],["Japan", "JP"],["Jordan", "JO"],["Kazakhstan", "KZ"],["Kenya", "KE"],["Kiribati", "KI"],["Kuwait", "KW"],["Kyrgyzstan", "KG"],["Lao People\'s Democratic Republic", "LA"],["Latvia", "LV"],["Lebanon", "LB"],["Lesotho", "LS"],["Liberia", "LR"],["Libyan Arab Jamahiriya", "LY"],["Liechtenstein", "LI"],["Lithuania", "LT"],["Luxembourg", "LU"],["Madagascar", "MG"],["Malawi", "MW"],["Malaysia", "MY"],["Maldives", "MV"],["Mali", "ML"],["Malta", "MT"],["Marshall Islands", "MH"],["Martinique", "MQ"],["Mauritania", "MR"],["Mauritius", "MU"],["Mayotte", ""],["Mexico", "MX"],["Micronesia (Federated States of)", "FM"],["Monaco", "MC"],["Mongolia", "MN"],["Montserrat", "MS"],["Morocco", "MA"],["Mozambique", "MZ"],["Myanmar", "MM"],["Namibia", "NA"],["Nauru", "NR"],["Nepal", "NP"],["Netherlands", "NL"],["Netherlands Antilles", "AN"],["New Caledonia", "NC"],["New Zealand", "NZ"],["Nicaragua", "NI"],["Niger", "NE"],["Nigeria", "NG"],["Niue", "NU"],["Norfolk Island", "NF"],["Northern Mariana Islands", "MP"],["Norway", "NO"],["Occupied Palestinian Territory", ""],["Oman", "OM"],["Pakistan", "PK"],["Palau", "PW"],["Panama", "PA"],["Papua New Guinea", "PG"],["Paraguay", "PY"],["Peru", "PE"],["Philippines", "PH"],["Pitcairn", "PN"],["Poland", "PL"],["Portugal", "PT"],["Puerto Rico", "PR"],["Qatar", "QA"],["Republic of Korea", "KR"],["Republic of Moldova", "MD"],["R�union", "RE"],["Romania", "RO"],["Russian Federation", "RU"],["Rwanda", "RW"],["Saint Helena", "SH"],["Saint Kitts and Nevis", "KN"],["Saint Lucia", "LC"],["Saint Pierre and Miquelon", "PM"],["Saint Vincent and the Grenadines", "VC"],["Samoa", "WS"],["San Marino", "SM"],["Sao Tome and Principe", "ST"],["Saudi Arabia", "SA"],["Senegal", "SN"],["Serbia and Montenegro", "RS"],["Seychelles", "SC"],["Sierra Leone", "SL"],["Singapore", "SG"],["Slovakia", "SK"],["Slovenia", "SI"],["Solomon Islands", "SB"],["Somalia", "SO"],["South Africa", "ZA"],["Spain", "ES"],["Sri Lanka", "LK"],["Sudan", "SD"],["Suriname", "SR"],["Svalbard and Jan Mayen Islands", "SJ"],["Swaziland", "SZ"],["Sweden", "SE"],["Switzerland", "CH"],["Syrian Arab Republic", "SY"],["Taiwan", "TW"],["Tajikistan", "TJ"],["Thailand", "TH"],["The former Yugoslav Rep. of Macedonia", "MK"],["Timor-Leste", "TL"],["Togo", "TG"],["Tokelau", "TK"],["Tonga", "TO"],["Trinidad and Tobago", "TT"],["Tunisia", "TN"],["Turkey", "TR"],["Turkmenistan", "TM"],["Turks and Caicos Islands", "TC"],["Tuvalu", "TV"],["Uganda", "UG"],["Ukraine", "UA"],["United Arab Emirates", "AE"],["United Kingdom", "GB"],["United Republic of Tanzania", "TZ"],["United States", "US"],["United States Virgin Islands", "VI"],["Uruguay", "UY"],["Uzbekistan", "UZ"],["Vanuatu", "VU"],["Venezuela", "VE"],["Viet Nam", "VN"],["Wallis and Futuna Islands", "WF"],["Western Sahara", "EH"],["Yemen", "YE"],["Zambia", "ZM"],["Zimbabwe", "ZW"]]
 
+const email_regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, 
+phone_regex = /1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})/,
+zip_regex = /^\d{5}$/;
+
 export default class NameAddressForm extends Component {
     constructor(props){
         super(props)
@@ -103,6 +107,7 @@ export default class NameAddressForm extends Component {
             }
         }
         this.handleInputChange = this.handleInputChange.bind(this)
+        this.validateInput = this.validateInput.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleRadioClick = this.handleRadioClick.bind(this)
         this.renderMonthlyRadio = this.renderMonthlyRadio.bind(this)
@@ -360,11 +365,16 @@ export default class NameAddressForm extends Component {
         const target = e.target;
         let value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
+
+        const errors = this.state.errors;
+
+        const error = this.validateInput(name, value);
+        errors[name] = error;
         
         const fields = this.state.fields;
         fields[name] = value;
 
-        this.setState({ fields });
+        this.setState({ fields, errors });
     }
 
     handleShippingClick(e) {
@@ -493,6 +503,46 @@ export default class NameAddressForm extends Component {
     validateInput(name, value) {
         let error = '';
         switch(name) {
+            case "EmailAddress":
+                if (value && !(email_regex.test(value))){
+                    error = "Please enter a valid email: ie. you@example.com"
+                } 
+                break;
+            case "phone" :
+                if (value && !(phone_regex.test(value))){
+                    error = "Please enter a valid phone number, numbers only: ie. 7575551212"
+                }
+                break;
+            case "Zip":
+            case "ShipToZip":
+                const country = name == "ShipToZip" ? "ShipToCountry" : "Country";
+                if (value && this.state.fields[country] == "US" && !(zip_regex.test(value))) {
+                    error = "Please enter a valid US Zip Code"
+                } 
+                if (value && this.state.fields[country] == "US" && zip_regex.test(value)) {
+                    axios.get(`http://Services.cbn.local/AddressValidation/CityStatebyZip.aspx?PostalCode=${value}`)
+                        .then(response=>console.log(response.data))
+                        .catch(error=>{
+                            if (error.response) {
+                                // The request was made and the server responded with a status code
+                                // that falls out of the range of 2xx
+                                console.error(error.response.data);
+                                console.error(error.response.status);
+                                console.error(error.response.headers);
+                            } else if (error.request) {
+                                // The request was made but no response was received
+                                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                                // http.ClientRequest in node.js
+                                console.error(error.request);
+                            } else {
+                                // Something happened in setting up the request that triggered an Error
+                                console.error('Error', error.message);
+                            }
+                            console.error({error})
+                            console.error(error.config);
+                        });
+                }
+                break;
 
         }
         return error
