a2afa5c1c4a7b5dd89cba1eb734002de6acdd06f
wesleylhandy <wesleylhandy@gmail.com>
2018-07-11 17:21:35 -0400

    new branch, changed confirmation process to iframed process

--- src/Components/App.js
+++ src/Components/App.js
@@ -96,35 +96,10 @@ export default class App extends Component {
     }
 
     submitForm({msg, data}) {
-        const donorID = msg.split(";")[0].split(" - ")[1]
-        const confirmURI = msg.split(" is ")[1]
-        const bodyFormData = new FormData();
-        bodyFormData.set('DonorID', donorID);
-        /*bodyFormData.set('ApiKey', data.APIaccessID);
-        bodyFormData.set('BackColor', '#ED7014');
-        bodyFormData.set('HeaderFile', 'http://pre.vb.cbn.local/giving/api/TestFormHeader.htm');
-        bodyFormData.set('FooterFile', 'http://pre.vb.cbn.local/giving/api/TestFormFooter.htm');*/
-        axios({
-            method: 'POST',
-            url: confirmURI,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data:  bodyFormData
-        }).then(response=> {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(response.data, "text/html");
-            const formAction = doc.querySelector('form').action
-            const inputs = doc.querySelectorAll('input[type="hidden"]')
-            const confirmationData = []
-            inputs.forEach(input=> confirmationData.push({name: input.name, value: input.value}))
-            // console.log(confirmationData)
-            this.setState({submitted: true, formData: data, donorID, confirmationData, formAction});
-        }).catch(error=>{
-            logError(error)
-            this.setState({submitting: false})
-        })
-
+        const DonorID = msg.split(";")[0].split(" - ")[1]
+        const formAction = msg.split(" is ")[1]
+        data.DonorID = DonorID;
+        this.setState({submitted: true, formData: data, formAction});
     }
 
     hydrateForm(data){
@@ -138,10 +113,8 @@ export default class App extends Component {
                 { 
                     this.state.submitted ? ( 
                         <ConfirmationPage 
-                            confirmationData={this.state.confirmationData} 
                             formData={this.state.formData} 
                             formAction={this.state.formAction}
-                            hydrateForm={this.hydrateForm}
                         /> 
                     ) : ( 
                         <NameAddressForm {...this.state } submitForm={ this.submitForm }/> 
