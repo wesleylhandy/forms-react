77935d68d774a27bf2f8f5b5e75242929078ae01
wesleylhandy <wesleylhandy@gmail.com>
2019-03-27 13:41:22 -0400

    add donor information to thank you page, add padding to form-control inputs

--- dist/react.545e1cc3.js
+++ dist/react.545e1cc3.js
@@ -2061,7 +2061,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65324" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54260" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
